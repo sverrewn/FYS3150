dbc9267cd0673a098f9742b9208b3d01f0d6a69c
sverrwn <sverrwn@uio.no>
2021-09-05 22:19:22 +0200

    små (undøvendige) optimaliseringer

--- project1/poisson_exact.cpp
+++ project1/poisson_exact.cpp
@@ -1,7 +1,6 @@
 #include <cmath>
 #include <fstream>
 #include <iomanip>
-#include <iostream>
 #include <vector>
 
 double u(double);
@@ -10,30 +9,33 @@ int main()
 {
     std::vector<double> x_vals, result;
 
+    x_vals.reserve(1002);
+    result.reserve(1002);
+
     for ( int i = 0; i < 1002; ++i ) {
         x_vals.push_back(i / 1001.0);
-    }
-
-    for ( double x: x_vals ) {
-        result.push_back(u(x));
+        result.push_back(u(i));
     }
 
     std::ofstream file;
     file.open("poisson_exact.dat");
 
-    for ( double x: x_vals) {
-        file << x << ",";
+    for ( int i = 0; i < 1002 - 1; i++ ) {
+        file << x_vals[i] << ",";
     }
-    file << std::endl;
-    for ( double x: result) {
-        file << std::scientific << std::setprecision(4) << x << ",";
+    file << x_vals[1001] << std::endl;
+
+    for ( int i; i < 1002 - 1; ++i ) {
+        file << std::scientific << std::setprecision(4) << result[i] << ",";
     }
-    file << std::endl;
+    file << result[1001] << std::endl;
 
     return 0;
 }
 
+
 double u(double x)
 {
-    return ( 1 - (1 - exp(-10)) * x - exp(-10 * x) );
+    static const double u = exp(-10);
+    return ( 1 - (1 - u) * x - exp(-10 * x) );
 }
