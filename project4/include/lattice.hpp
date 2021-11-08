239dfa7265f519f3f2f9d0874d303a7ffd45c78f
Sverre W. Normesaune <sverrwn@uio.no>
2021-11-08 13:40:42 +0100

    work on prob 4

--- project4/include/lattice.hpp
+++ project4/include/lattice.hpp
@@ -6,10 +6,16 @@
 class Lattice {
 private:
     int length;
+    int temperature;
+    int N;
+    int J;
+    float kb;
     arma::Mat<short> lattice;
 public:
-    Lattice(int L);
+    Lattice(int L, float T,);
     int length();
+
+    float energy_per_spin();
 };
 
 #endif
--- project4/src/lattice.cpp
+++ project4/src/lattice.cpp
@@ -1,16 +1,80 @@
 #include <armadillo>
+#include <cmath>
 
 #include "lattice.hpp"
 
 
-
-Lattice::Lattice()
-{
-
+Lattice::Lattice(int L, float T)
+{   
+    length = L;
+    temprature = T;
+    N = length * length;
+    J = -1;
+    float kb = 1
+    lattice(L+1, L+1);
 }
 
 
 Lattice::length()
 {
     return length;
+}
+
+
+void Lattice::init()
+{
+
+}
+
+
+void Lattice::advance()
+{
+
+}
+
+
+int Lattice::total_energy()
+{
+    int sum = 0;
+
+    for ( int i = 0; i < length; ++i ) {
+        for ( int j = 0; i < length; ++j ) {
+            sum += lattice(i,j) * lattice(i+1,j) + lattice(i,j) *lattice(i,j+1);
+        }
+    }
+
+    return ( J * sum)
+}
+
+
+float Lattice::energy_per_spin()
+{
+    return ( static_cast<float>(total_energy()) / static_cast<float>(N) );
+}
+
+
+int Lattice::total_magnetization()
+{
+    int sum = 0;
+
+    for ( int i = 0; i < length; ++i ) {
+        for ( int j = 0; j < length; ++j ) {
+            sum += lattice(i,j);
+        }
+    }
+}
+
+
+float Lattice::magnetization_per_spin()
+{
+    return std::abs( static_cast<float>(total_magnetization()) / static_cast<float>(N) );
+}
+
+
+float Lattice::heat_capacity()
+{
+    float temp = 1.0 / ( kb * T * T);
+    e = energy_per_spin();
+    e2 = ;
+    return temp * (e2 - e * e);
 }
