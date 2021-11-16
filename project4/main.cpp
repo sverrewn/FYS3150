f9062a46298e6478b13987f5254591e13ce5e89e
jonaproitz <jonaproitz@gmail.com>
2021-11-16 13:47:48 +0100

    Calculate possible boltzman constants

--- project4/main.cpp
+++ project4/main.cpp
@@ -8,8 +8,20 @@ std::vector<int> Delta_E_values(){
     return {-8, -4, 0, 4, 8};
 }
 
+std::vector<double> calculate_possible_boltzman_factors(std::vector<int> &possible_Delta_E_values, double &beta){
+    // Find possible values for the Boltzman factor
+    std::vector<double> possible_Boltzman_factors;
+    for (int i=0; i<possible_Delta_E_values.size(); i++){
+        possible_Boltzman_factors.push_back(exp(-beta*(-possible_Delta_E_values[i])));
+    }
+    return possible_Boltzman_factors;
+}
+
 int main(){
+    double beta = 1;
     std::vector<int> possible_Delta_E_values = Delta_E_values();
+    std::vector<double> possible_Boltzman_factors = calculate_possible_boltzman_factors(possible_Delta_E_values, beta);
+
 
     return 0;
 }
