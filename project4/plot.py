b61d515e97ceafc094eb0e8be96ee4e5f390638c
jonaproitz <jonaproitz@gmail.com>
2021-11-21 15:06:52 +0100

    Show data structure

--- project4/plot.py
+++ project4/plot.py
@@ -15,3 +15,19 @@ while True:
         data = data.T
         break
 
+"""
+Data structure:
+data[0] = Ordered
+data[1] = Cycles
+data[2] = Temperature
+data[3] = Current E
+data[4] = Current M
+data[5] = <E>
+data[6] = <E^2>
+data[7] = <M>
+data[8] = <M^2>
+data[9] = <|M|>
+data[10] = Cv
+data[11] = X
+"""
+
