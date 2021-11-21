9295b988e2ff15b9fa6bf59077e2ab7beb872c44
jonaproitz <jonaproitz@gmail.com>
2021-11-21 15:01:21 +0100

    Store data correctly

--- project4/plot.py
+++ project4/plot.py
@@ -3,5 +3,15 @@ import matplotlib.pyplot as plt
 from sys import argv
 
 
-data = np.loadtxt(argv[1])
+data = [np.loadtxt(argv[1])]
+i = 2
+while True:
+    try:
+        tmp = np.loadtxt(argv[i])
+        data = np.append(data, [tmp], axis=0)
+        i += 1
+
+    except:
+        data = data.T
+        break
 
