f562fd356aad45bd8f1335aac17cc71b2000137c
Sverre W. Normesaune <sverrwn@uio.no>
2021-11-29 16:58:40 +0100

    I HAVE HAD THOUGHTS. I DO NOT HAVE THE ENERGY TO FINISH THINKING THEM. GOODBYE

---
+++ project5/src/snippets.cpp
@@ -0,0 +1,22 @@
+inline int translate_index(int i, int j, int length)
+{
+    return ( i * length + j);
+}
+
+
+void create_matrices(arma::mat& A, arma::mat& B, int r, arma::vec& a, arma::vec& b, int sub_len)
+{
+    int tot_len = sub_len * sub_len;
+    
+    // 0,0
+
+    for 
+
+    for ( int i = 1; i < sub_len - 1; ++i ) {
+        for ( int j = i - 1; j <= i + 1; ++j ) {
+
+        }
+    }
+
+    // sub_len, sub_len
+}
