bd584cfa230b65f6664dd4012f8587bfdc9400f6
jonaproitz <jonaproitz@gmail.com>
2021-12-11 16:48:30 +0100

    Prettify plotting

--- project5/src/plot.py
+++ project5/src/plot.py
@@ -3,21 +3,17 @@ import matplotlib.pyplot as plt
 
 import pyarma as pa
 
+def deviation(filename):
+    U = pa.cx_cube()
+    U.load(filename)
+    U = np.array(U).reshape(np.shape(U)[0], np.shape(U)[1] * np.shape(U)[2])
 
-U_1 = pa.cx_cube()
-U_2 = pa.cx_cube()
-
-U_1.load("data/run1.bin")
-U_2.load("data/run2.bin")
-U_1 = np.array(U_1).reshape(np.shape(U_1)[0], np.shape(U_1)[1] * np.shape(U_1)[2])
-U_2 = np.array(U_2).reshape(np.shape(U_2)[0], np.shape(U_2)[1] * np.shape(U_2)[2])
-
-p_1 = np.sum(np.real(U_1*np.conj(U_1)), axis=1)
-p_2 = np.sum(np.real(U_2*np.conj(U_2)), axis=1)
-
-
-
+    p = np.sum(np.real(U*np.conj(U)), axis=1)
+    
+    plt.plot(np.log10(p - 1))
+    
 plt.figure()
-plt.plot(np.log10(p_1 - 1))
-plt.plot(np.log10(p_2 - 1))
+deviation("data/run1.bin")
+deviation("data/run2.bin")
 plt.show()
+
