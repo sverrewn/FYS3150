073251a80e342e93afd07472b5a465d65b48dba3
jonaproitz <jonaproitz@gmail.com>
2021-12-11 16:35:51 +0100

    Change sig_y value of run 2

--- project5/run.sh
+++ project5/run.sh
@@ -2,8 +2,8 @@ mkdir data
 
 make main
 
-./simulation.out 0.005 0.000025 0.008 0.25 0.05 200 0.5 0.005 0 0 0 "data/run1.txt"
+./simulation.out 0.005 0.000025 0.008 0.25 0.05 200 0.5 0.005 0 0 0 "data/run1.bin"
 
-./simulation.out 0.005 0.000025 0.008 0.25 0.05 200 0.5 0.005 0 10000000000 2 "data/run2.txt"
+./simulation.out 0.005 0.000025 0.008 0.25 0.05 200 0.5 0.1 0 10000000000 2 "data/run2.bin"
 
-#./simulation.out 0.005 0.000025 0.008 0.25 0.05 200 0.5 0.005 0 0 0 "data/run3.txt"
+#./simulation.out 0.005 0.000025 0.008 0.25 0.05 200 0.5 0.005 0 0 0 "data/run3.bin"
